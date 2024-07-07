d81a797396c912d088fa8454cc9ce0909aad1c8e
ThatOneRR <ronaldpangrenda@gmail.com>
2024-07-07 15:26:55 +0800

    Implemented resizable panels in the calendar area
    CSS method resize: horizontal uses pixels instead of percentage and can only be resized by holding the bottom right handle, so custom JS solution is the preferred method for now instead

--- Frontend/Pages/js/letmecook.js
+++ Frontend/Pages/js/letmecook.js
@@ -1,5 +1,10 @@
 var isNavToggled = false;
+var isDraggingDivider = false;
 var counter = 0;
+
+document.addEventListener("DOMContentLoaded", (domLoadEvent) => {
+    panelResizeListener();
+});
 
 function onNavToggleBtnClick(clickEvent) {
     var navBar = document.getElementById("nav");
@@ -23,4 +28,33 @@ function onNavToggleBtnClick(clickEvent) {
 function calendarLayout() {
     console.log("shes a burden for " + counter + "times");
     counter++;
+}
+
+/**
+ * Controls the resizing behaviour of the left and right windows in the main panel
+ * 
+ * Issue: If resizing is too fast, cursor will highlight text it moves over briefly
+ * 
+ * @param {Number} leftPanelMinWidthPerc The minimum width (in %) that the left panel can have
+ * @param {Number} leftPanelMaxWidthPerc The maximum width (in %) that the left panel can have
+ */
+function panelResizeListener(leftPanelMinWidthPerc = 30, leftPanelMaxWidthPerc = 80) {
+    const mainPanel = document.getElementById("mainArea").getElementsByClassName("main")[0], leftPanel = mainPanel.getElementsByClassName("left")[0], resizer = mainPanel.getElementsByClassName("resizer")[0];
+
+    resizer.addEventListener("mousedown", (mouseEvent) => { isDraggingDivider = true; });
+    document.addEventListener("mouseup", (mouseEvent) => { isDraggingDivider = false; });
+    document.addEventListener("mousemove", (mouseEvent) => {
+        if (!isDraggingDivider) return;
+        mouseEvent.preventDefault();
+        const mainPanelWidth = parseInt(document.defaultView.getComputedStyle(mainPanel).width);
+        
+        /*
+            #1: Math.min(Math.max(x, minBound), maxBound) performs a clamp so the returned value is always within the boundaries
+
+            #2: mouseEvent.clientX returns how many px the cursor is from left of screen
+
+            #3: document.documentElement.clientWidth / 10 is equal to "10vw" (clientWidth = 100vw). This value is subtracted to ensure the left margin of the main panel is taken into account
+        */ 
+        leftPanel.style.width = Math.min(Math.max(((mouseEvent.clientX - document.documentElement.clientWidth / 10) * 100 / mainPanelWidth), leftPanelMinWidthPerc), leftPanelMaxWidthPerc) + "%";
+    });
 }
