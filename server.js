ce501321eeef7ceba300246205c839001955fb8d
GrassHeadd <113354001+GrassHeadd@users.noreply.github.com>
2024-12-13 21:12:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { createClient } from '@supabase/supabase-js';
 // Load environment variables
 dotenv.config();  
 const supabaseUrl = 'https://rmjcnufjtkakbcocvglf.supabase.co';
-const supabaseKey = process.env.DB_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 
