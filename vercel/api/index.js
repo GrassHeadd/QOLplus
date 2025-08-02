9aea1e4ff69b7433f945aedc45d95798da969f04
ThatOneRR <ronaldpangrenda@gmail.com>
2025-08-02 23:06:54 +0800

    Debug test for Vercel Functions migration 19

--- vercel/api/index.js
+++ vercel/api/index.js
@@ -1,5 +1,16 @@
+// const serverless = require("serverless-http");
+
+// const app = require("../../backend/server.js");
+
+// module.exports = serverless(app);
+
+const express = require("express");
 const serverless = require("serverless-http");
 
-const app = require("../../backend/server.js");
+const app = express();
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong!" });
+});
 
 module.exports = serverless(app);
