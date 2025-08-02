9a1dcdcf150a58284e7d3a1f7bc6e51c7bee8032
ThatOneRR <ronaldpangrenda@gmail.com>
2025-08-02 22:57:31 +0800

    Debug test for Vercel Functions migration 18

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
+// const bodyParser = require("body-parser");
+// const dotenv = require("dotenv");
 const cors = require("cors");
 
-// Load environment variables
-dotenv.config();
+// // Load environment variables
+// dotenv.config();
 
-// Individual sub-files
-const events = require("./routing/events.js");
-const exercises = require("./routing/exercises.js");
-// const food = require("./backend/routing/food.js");
-const expenses = require("./routing/expenses.js");
+// // Individual sub-files
+// const events = require("./routing/events.js");
+// const exercises = require("./routing/exercises.js");
+// // const food = require("./backend/routing/food.js");
+// const expenses = require("./routing/expenses.js");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(cors());
 
-app.use("/api/events", events);
-app.use("/api/exercises", exercises);
-// app.use("/food", food);
-app.use("/api/expenses", expenses);
+// app.use("/api/events", events);
+// app.use("/api/exercises", exercises);
+// // app.use("/food", food);
+// app.use("/api/expenses", expenses);
 
 app.get("/api/hello", (req, res) => {
   res.status(200).json({ message: "Hello from server!" });
