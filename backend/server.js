91896f3ed3db1d9c049a55c9e4bcd62a2784de4d
ThatOneRR <ronaldpangrenda@gmail.com>
2025-08-02 16:07:58 +0800

    Project file cleanup for backend 6

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,10 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use("/events", events);
-app.use("/exercises", exercises);
+app.use("/api/events", events);
+app.use("/api/exercises", exercises);
 // app.use("/food", food);
-app.use("/expenses", expenses);
+app.use("/api/expenses", expenses);
 
 // Format: YYYYMMDD
 //TODO
