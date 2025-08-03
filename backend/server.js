ec54c64540a617c9fd638ad525b104f0184091a7
ThatOneRR <ronaldpangrenda@gmail.com>
2025-08-03 11:33:05 +0800

    Cleanup for working Vercel Functions migration

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,11 @@ const cors = require("cors");
 // // Load environment variables
 dotenv.config();
 
-// // Individual sub-files
+// Individual sub-files
 const events = require("./routing/events.js");
-// const exercises = require("./routing/exercises.js");
-// // const food = require("./backend/routing/food.js");
-// const expenses = require("./routing/expenses.js");
+const exercises = require("./routing/exercises.js");
+// const food = require("./backend/routing/food.js");
+const expenses = require("./routing/expenses.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,9 +19,9 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/api/events", events);
-// app.use("/api/exercises", exercises);
-// // app.use("/food", food);
-// app.use("/api/expenses", expenses);
+app.use("/api/exercises", exercises);
+// app.use("/food", food);
+app.use("/api/expenses", expenses);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- vercel/api/index.js
+++ vercel/api/index.js
@@ -1,34 +1,3 @@
 const app = require("../../backend/server.js");
 
 module.exports = app;
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-
-// dotenv.config();
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// app.use(bodyParser.json());
-// app.use(cors());
-
-// app.get("/api/hello", (req, res) => {
-//   res.status(200).json({ message: "Hello from server!" });
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-// module.exports = app;
-
-// module.exports = (request, response) => {
-//   if (request.method === "GET") {
-//     response.status(200).json({ message: "Hello from Vercel!" });
-//   } else {
-//     response.status(405).json({ message: "Method not allowed" });
-//   }
-// };
