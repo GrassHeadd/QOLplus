dbc46d2bfecf43bda48fe69189abadd2bce9a595
ThatOneRR <ronaldpangrenda@gmail.com>
2025-08-03 11:20:54 +0800

    Debug test for Vercel Functions migration 33

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,30 @@
 const express = require("express");
-const serverless = require("serverless-http");
-// const bodyParser = require("body-parser");
-// const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 const cors = require("cors");
 
 // // Load environment variables
-// dotenv.config();
+dotenv.config();
 
 // // Individual sub-files
-// const events = require("./routing/events.js");
+const events = require("./routing/events.js");
 // const exercises = require("./routing/exercises.js");
 // // const food = require("./backend/routing/food.js");
 // const expenses = require("./routing/expenses.js");
 
 const app = express();
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(cors());
 
-// app.use("/api/events", events);
+app.use("/api/events", events);
 // app.use("/api/exercises", exercises);
 // // app.use("/food", food);
 // app.use("/api/expenses", expenses);
 
-app.get("/api/hello", (req, res) => {
-  res.status(200).json({ message: "Hello from server!" });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
-module.exports = serverless(app);
+module.exports = app;
--- vercel/api/index.js
+++ vercel/api/index.js
@@ -1,25 +1,29 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
-const cors = require("cors");
-
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(bodyParser.json());
-app.use(cors());
-
-app.get("/api/hello", (req, res) => {
-  res.status(200).json({ message: "Hello from server!" });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const app = require("../../backend/server.js");
 
 module.exports = app;
+
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const dotenv = require("dotenv");
+// const cors = require("cors");
+
+// dotenv.config();
+
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// app.get("/api/hello", (req, res) => {
+//   res.status(200).json({ message: "Hello from server!" });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+// module.exports = app;
 
 // module.exports = (request, response) => {
 //   if (request.method === "GET") {
